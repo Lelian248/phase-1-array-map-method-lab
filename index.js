6276caada550c3c1016d37d9bf05b03ed2b5cb70
lilian manaa <lelian.ibraheem@gmail.com>
2022-10-10 18:04:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -23,38 +23,3 @@ const titleCased = () => {
 }
 
 console.log(titleCased()) 
-
- /* 
-const t = 'what does the this keyword mean?'
-
-const question1 = t.split(" ").toString().replace(/^./, t[0].toUpperCase())
-/*
-const question = tutorials.map(tutorial =>{
-  tutorial.charAt(0).toUpperCase() + tutorial.slice(1).join(" ")
-
-const test1=tutorials.map(tutorial =>{
-  tutorial.split(" ").map(word=>word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
-})
-const test2=tutorials.map(tutorial =>{
-  tutorial.split(" ")
-})
-const result = t.split(" ").map(word=>word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
-const test = 'what'
-const r2=test.charAt(0).toUpperCase() + test.slice(1);
-
-console.log(question)
-
-console.log(result)
-console.log(r2)
-console.log(test1)
-console.log(test2)
-
-const array =[]
-for(let i=0;i<tutorials.length;i++){
-  const p=tutorials[i].split(" ").map(word=>word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
-  console.log(p)
-  array.push(p)
-  
-}
-console.log(array) */
-//console.log(question1)
